3b6f4ccaed9d249567e40307b7403c97e82e3e2c
Camden DeVoogd <camdendevoogd@gmail.com>
2024-04-28 20:46:43 -0600

    Automatically check the config on startup

--- rsnapshot/entrypoint.sh
+++ rsnapshot/entrypoint.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
 
 crontab_file=/var/spool/cron/crontabs/root
+rsnapshot_config="/etc/rsnapshot.conf"
+
 echo "Configured root crontab ($crontab_file)"
 cat "$crontab_file"
 echo
+
+if [ -f "$rsnapshot_config" ]; then
+    echo "Found a config at rsnapshot's default config location: $rsnapshot_config"
+    echo "Running configtest to ensure the config file is valid"
+    if ! rsnapshot configtest; then
+        echo "'rsnapshot configtest' returning a non-zero code - the config is invalid"
+        exit 1
+    fi
+fi
 
 echo "Starting crond in the foreground"
 crond -f -L /dev/stdout
